f1f69d64fb4687983a14c4a4db69885675a8bd70
Chistophe Schnuerer <christophe.schnuerer@pdfforge.org>
2023-05-15 19:21:09 +0200

    PDFCreator 5.1.1

---
+++ Source/Application/UI/Presentation/Converter/MenuTabColumnWidthConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace pdfforge.PDFCreator.UI.Presentation.Converter
+{
+    public class MenuTabColumnWidthConverter : IValueConverter
+    {
+        public int TrueValue { get; set; }
+        public int FalseValue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is bool b))
+                return null;
+            return b ? TrueValue : FalseValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
